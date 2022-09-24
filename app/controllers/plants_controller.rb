48f4fb1408ae99bd10e8791d5a44428a5bf6d7d1
koechronix <koechronix@yahoo.com>
2022-09-24 20:47:15 +0300

    Added refactoring method to avoid DRY

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,42 @@
+# class PlantsController < ApplicationController
+#     # GET /plants
+#   def index
+#     plants = Plant.all
+#     render json: plants
+#   end
+
+#   # GET /plants/:id
+#   def show
+#     plant = Plant.find_by(id: params[:id])
+#     render json: plant
+#   end
+
+#   # POST /plants
+#   def create
+#     plant = Plant.create(plant_params)
+#     render json: plant, status: :created
+#   end
+
+#   private
+
+#   def plant_params
+#     params.permit(:name, :image, :price)
+#   end
+# end
+
+# BOTH METHODS WORKS
+
+# STEP 1:add reponse if the plant is not found 
+# STEP 2:define in private "render_reponse_not_found and find_plant"
+# STEP 3:replace accodingly to find_plant and render_response_not_found
+# STEP 4: add rescue ActiveRecord::RecordNotFound and run in postman it and watch
+# STEP 5: add rescue ActiveRecord::RecordNotFound,with....at the top to wrap up and clear
+
+
+
+
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     # GET /plants
   def index
     plants = Plant.all
@@ -7,9 +45,10 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
+  
 
   # POST /plants
   def create
@@ -22,4 +61,15 @@ class PlantsController < ApplicationController
   def plant_params
     params.permit(:name, :image, :price)
   end
+
+  def render_not_found_response
+    render json: { error: "Plant not found"}, status: :not_found
+  end
+
+  def find_plant
+    plant = Plant.find_by!(id: params[:id]) 
+  end
+
 end
+
+
